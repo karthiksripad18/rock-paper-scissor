2f160df7f1efe6d3aca189221f4d9d3d17e63566
karthiksripad18 <karthiksripad18@gmail.com>
2021-05-04 19:21:13 +0530

    added router

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,13 @@ import './index.css';
 import App from './App';
 import store from './redux/store';
 import {Provider} from 'react-redux';
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <Provider store ={store}><App /></Provider>,
+  <Provider store ={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
